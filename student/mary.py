562bc9eac3897f7f040168bc97fe548118b3bee2
WaiYanLinn10 <linchanglong.wyl1110@gmail.com>
2020-03-06 12:12:11 +0630

    fibonacci, result, student, mary

---
+++ student/mary.py
@@ -0,0 +1,6 @@
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+
+
+
+# Create "Student" Folder into Python Code
+# Write these three python files.
---
+++ student/result.py
@@ -0,0 +1,19 @@
+import student
+import mary
+
+x = student.a["code"]
+y = student.b["code"]
+z = student.a["student"]
+print(x , ' ', y, ' ', z)
+
+for i in range(len(mary.a)):
+	print(i, mary.a[i])
+
+t = student.a["student"]
+u = student.a["age"]
+v = student.a["mother"]
+x = student.b["student"]
+w = student.a["father"]
+print(t , ' ', u, ' ', v, ' ',x, ' ', w)
+
+	
