8af5dca1e927f1f69649b0beab4e4c8d52a0a904
WaiYanLinn10 <linchanglong.wyl1110@gmail.com>
2020-03-07 12:04:58 +0630

    pip

---
+++ result.py
@@ -0,0 +1,17 @@
+import studentx
+import maryo
+
+x = studentx.a["code"]
+y = studentx.b["code"]
+z = studentx.a["student"]
+print(x , ' ', y, ' ', z)
+
+for i in range(len(maryo.a)):
+	print(i, maryo.a[i])
+
+t = studentx.a["student"]
+u = studentx.a["age"]
+v = studentx.a["mother"]
+x = studentx.b["student"]
+w = studentx.a["father"]
+print(t , ' ', u, ' ', v, ' ',x, ' ', w)
